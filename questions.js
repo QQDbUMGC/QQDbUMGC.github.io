3e3548252e33f51655d4a649477ae10778fc2c5e
QQDbUMGC <153712755+QQDbUMGC@users.noreply.github.com>
2023-12-13 16:23:15 +0800

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,19 +1,9 @@
 var questions = [
     { question: "How many types of stem cells are there?",
-      answer: "
-          <bold>Totipotent (or Omnipotent) Stem Cells</bold>: These are the most potent stem cells, capable of differentiating into any of the 220 cell types found in an embryo as well as extra-embryonic cells (like the placenta). An example of totipotent cells is the zygote, the fertilized egg.<br>
-
-Pluripotent Stem Cells: These stem cells can give rise to all cell types of the body (but not the placenta). Examples of pluripotent stem cells include embryonic stem (ES) cells and induced pluripotent stem (iPS) cells.<br>
-
-Multipotent Stem Cells: These stem cells can develop into a limited number of cell types in a particular lineage1. An example of multipotent stem cells is hematopoietic stem (HS) cells, which can differentiate into various types of blood cells.<br>
-
-Oligopotent Stem Cells: These stem cells can differentiate into a few closely related cell types5. An example would be lymphoid or myeloid stem cells, which are believed to originate from HSCs, in the bone marrow.<br>
-
-Unipotent Stem Cells: These stem cells can produce only one cell type, their own, but have the property of self-renewal, which distinguishes them from non-stem cells5. An example would be muscle stem cells.
-          ", 
+      answer: "1) Totipotent (or Omnipotent) Stem Cells: These are the most potent stem cells, capable of differentiating into any of the 220 cell types found in an embryo as well as extra-embryonic cells (like the placenta). An example of totipotent cells is the zygote, the fertilized egg. 2) Pluripotent Stem Cells: These stem cells can give rise to all cell types of the body (but not the placenta). Examples of pluripotent stem cells include embryonic stem (ES) cells and induced pluripotent stem (iPS) cells.3) Multipotent Stem Cells: These stem cells can develop into a limited number of cell types in a particular lineage1. An example of multipotent stem cells is hematopoietic stem (HS) cells, which can differentiate into various types of blood cells. 4) Oligopotent Stem Cells: These stem cells can differentiate into a few closely related cell types5. An example would be lymphoid or myeloid stem cells, which are believed to originated from HSCs, in the bone marrow. 5) Unipotent Stem Cells: These stem cells can produce only one cell type, their own, but have the property of self-renewal, which distinguishes them from non-stem cells5. An example would be muscle stem cells.", 
       frequency: 1, 
       tags: ["cell", "linage", "basic"], 
-      imageUrl: "paris.jpg", 
+      imageUrl: "typesofstemcells.png", 
       occasion: "HCZ QE: Suspension culture and AKTi as Antibacterials" },
     // Add more questions here...
 ];
