7737864829cedfdc22884309b33d3051d373aa02
QQDbUMGC <153712755+QQDbUMGC@users.noreply.github.com>
2023-12-13 15:48:59 +0800

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,4 @@
+var questions = [
+    { question: "What is the capital of France?", answer: "Paris", time: "2023-12-13T14:53:49Z", tags: ["geography", "capitals"] },
+    // Add more questions here...
+];
