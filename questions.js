459e6dd209f159ed176500d7f51701b04f153b26
S. Wei <130944780+PeonyMuds@users.noreply.github.com>
2023-12-13 22:33:47 +0800

    Update questions.js

--- questions.js
+++ questions.js
@@ -2,16 +2,23 @@ var questions = [
     { question: "1. How many types of stem cells are there?", 
       answer: "<br>1) <b>Totipotent (or Omnipotent) Stem Cells</b>: These are the most potent stem cells, capable of differentiating into any of the 220 cell types found in an embryo as well as extra-embryonic cells (like the placenta). An example of totipotent cells is the zygote, the fertilized egg. <br>2) <b>Pluripotent Stem Cells</b>: These stem cells can give rise to all cell types of the body (but not the placenta). Examples of pluripotent stem cells include embryonic stem (ES) cells and induced pluripotent stem (iPS) cells.<br>3) <b>Multipotent Stem Cells</b>: These stem cells can develop into a limited number of cell types in a particular lineage1. An example of multipotent stem cells is hematopoietic stem (HS) cells, which can differentiate into various types of blood cells. <br>4) <b>Oligopotent Stem Cells</b>: These stem cells can differentiate into a few closely related cell types5. An example would be lymphoid or myeloid stem cells, which are believed to originate from HSCs, in the bone marrow. <br>5) <b>Unipotent Stem Cells</b>: These stem cells can produce only one cell type, their own, but have the property of self-renewal, which distinguishes them from non-stem cells5. An example would be muscle stem cells.", 
       frequency: 1, 
-      tags: ["basic","cellularbio"], 
+      tags: ["basic","cellbio"], 
       imageUrl: "imageassets/1.png", 
       occasion: "HCZ QE: Suspension Culture & AKTi4 as Antibacterials", 
       asker: "Prof. Tzu-Ming LIU" },
+    
     { question: "2. Could adherence and morphology affect ESC differentiation?", 
       answer: "<br>1. Research conducted by Dr. DCH and ZZY demonstrated that cells located near the edges of the ES colonies are more sensitive to induction. <br>2. Some biomaterial research pointed out mechanics could be used to regulate stem cell differentiation.", 
       frequency: 1, 
-      tags: ["cellularbio"], 
+      tags: ["cellbio"], 
       imageUrl:"imageassets/2.jpg", 
       occasion: "HCZ QE: Suspension Culture & AKTi4 as Antibacterials", 
       asker: "Prof. WANG Chunming" },
+    
+    { question: "3. Which organisms do not have mesoderm.", 
+      answer: "<br>Diploblastic organisms, such as jellyfish, coral, sea-pens, etc. ", 
+      frequency: 1, 
+      tags: ["basic","devbio"], 
+      occasion: "ZZY QE"},
     // Add more questions here...
 ];
