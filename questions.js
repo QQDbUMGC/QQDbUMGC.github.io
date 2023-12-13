89f4c3d2e02c41c1c6b030959d34ba0d77138292
QQDbUMGC <153712755+QQDbUMGC@users.noreply.github.com>
2023-12-13 17:00:30 +0800

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,4 +1,4 @@
 var questions = [
-    { question: "What is the capital of France?", answer: "Paris", frequency: 10, tags: ["geography", "capitals"], imageUrl: "paris.jpg", occasion: "Geography Quiz" },
+    { question: "What is the capital of France?", answer: "Paris", frequency: 10, tags: ["geography", "capitals"], imageUrl: "paris.png" },
     // Add more questions here...
 ];
