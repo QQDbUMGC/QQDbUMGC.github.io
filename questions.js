9a8dc7eb93e9bc93d95f501bba2b06bab5320eaf
S. Wei <130944780+PeonyMuds@users.noreply.github.com>
2023-12-14 14:40:32 +0800

    Update questions.js

--- questions.js
+++ questions.js
@@ -20,5 +20,42 @@ var questions = [
       frequency: 1, 
       tags: ["basic","devbio"], 
       occasion: "ZZY QE"},
+
+    { question: "4. What are the advantages and disadvantages of a differentiation method?", 
+      answer: "<br> These are some aspects to consider.<br><b>Efficiency</b>: How many of the hESCs are successfully differentiated into the desired cell type?<br><b>Purity</b>: Are the differentiated cells pure, or are there other cell types present?<br><b>Time</b>: How long does the differentiation process take?<br><b>Cost</b>: What are the costs associated with the differentiation method? This includes the cost of reagents, equipment, and labour.<br><b>Reproducibility</b>: Can the differentiation method be reliably reproduced, both within the same lab and in other labs?<br><b>Ethical considerations</b>: Are there any ethical issues associated with the method?",
+      frequency: 1,
+      tags: ["basic"], 
+      occasion: "FSC QE: Keratinocyte Differentiation",
+      asker: "Prof. WANG Chunming"},
+
+    { question: "5. What are the lineages that could be induced along the embryonic and extraembryonic differentiation?", 
+      answer: "<br><b>Endoderm</b>: Visceral tissues and organs such as lungs, livers, intestines, etc.<br><b>Mesoderm</b>: Bones, muscles, and cardiac tissues.<br><b>Ectoderm</b>: Skin and neural system.<br><b>Extraembryonic</b>: trophoblast, which further develops into placenta, extraembryonic endoderm, which further develops into yolk sac's lining, and extraembryonic mesoderm, which further develops into yolk sac, chorion, and allantois.", 
+      frequency: 1, 
+      tags: ["basic","devcell"], 
+      occasion: "FSC QE: Keratinocyte Differentiation",
+      asker: "Prof. XU Ren-he"},
+
+    { question: "6. What are the potential hurdles for cell therapy?", 
+      answer: "<br> These are some aspects to consider.<br> Immunogenicity, cell survival, tumorigenicity (teratoma for remaining PSCs, fibroma and neoplasia for MSCs), limitations in the form of cell preparations/administrations, and regulatory requirements.", 
+      frequency: 1, 
+      tags: ["tech"], 
+      occasion: "FSC QE: Keratinocyte Differentiation",
+      asker: "Prof. XU Ren-he"},
+
+    { question: "7. Could 3D culture help the differentiation?", 
+      answer: "<br> These are generally two forms of 3D cell culture, suspension and matrix-based.<br> Suspension culture is helpful to expand the cells and with the help of regulatory molecules endogenously secreted or from adjacent cells, differentiation into certain cell types or organoids is feasible.<br> Matrix-based 3D cell culture aims to provide ECM for cells and helps the administration of cells as patches or injectable preparations. Sometimes, mechanics or modification with regulatory molecules could regulate the differentiation.", 
+      frequency: 1, 
+      tags: ["tech"], 
+      occasion: "FSC QE: Keratinocyte Differentiation",
+      asker: "Prof. XU Ren-he"},
+
+    { question: "8. What cell fate could be induced by BMP4?", 
+      answer: "<br>
+    	<b>Trophoblasts</b><br><b>Mesodermal cells</b><br><b>Endodermal cells</b><br><b>Amniotic cells</b><br>The final differentiated types of cells depend on the timing and dosage.", 
+      frequency: 1, 
+      tags: ["tech"], 
+      occasion: "FSC QE: Keratinocyte Differentiation",
+      asker: "Prof. XU Ren-he"},
+    
     // Add more questions here...
 ];
